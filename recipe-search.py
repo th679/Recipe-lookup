72582a9170326d0c05a46874d069760fc006841b
th679 <th679@georgetown.edu>
2019-04-15 17:58:49 -0400

    fill ListBox

--- recipe-search.py
+++ recipe-search.py
@@ -22,11 +22,20 @@ def search_button_click():
     request_url = f"https://api.edamam.com/search?q={search_value}&app_id={app_id}&app_key={app_key}"
     response = requests.get(request_url)
     parsed_response = json.loads(response.text)
-    print(parsed_response)
+    recipe_list = []
+    recipes = parsed_response["hits"]
+    for recipe in recipes:
+        recipe_list.append(recipe["recipe"]["label"])
+    recipe_select_label = tkinter.Label(text="Select a recipe you would like to add from the dropdown:")
+    recipe_select = tkinter.Listbox()
+    i = 1
+    for recipe in recipe_list:
+        recipe_select.insert(i, recipe)
+        i = i + 1
+    recipe_select_label.pack()
+    recipe_select.pack()
 
 search_button = tkinter.Button(text="Search", command=search_button_click)
-
-
 
 
 
@@ -35,5 +44,8 @@ search.pack()
 
 search_button.pack()
 
+
+
 window.mainloop()
+
 
