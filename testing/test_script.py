345b81f27a9993457e313fceb4c557389ff92f76
th679 <th679@georgetown.edu>
2019-04-30 16:15:54 -0400

    Update testing

--- testing/test_script.py
+++ testing/test_script.py
@@ -4,8 +4,6 @@ import json
 import os
 import pytest
 
-from app.recipe-search import get_response
-
 CI_ENV = os.environ.get("CI") == "true"
 SKIP_REASON = "to avoid issuing requests from the CI server"
 
@@ -19,9 +17,19 @@ def test_keys():
     assert app_id != "Set an env variable named my_app_id"
     assert app_key != "Set an env variable named my_app_id"
 
+def get_response(search_value):
+    app_key = os.environ.get("my_app_key", "Set an env variable named my_app_id")
+    app_id = os.environ.get("my_app_id", "Set an env variable named my_app_id")
+    url = f"https://api.edamam.com/search?q={search_value}&app_id={app_id}&app_key={app_key}"
+    response = requests.get(url)
+    return json.loads(response.text)
 
+@pytest.mark.skipif(CI_ENV==True, reason=SKIP_REASON)
 def test_search():
     search_value = "chicken"
     parsed_response = get_response(search_value)
+    recipes = parsed_response["hits"]
     assert isinstance(parsed_response, dict)
+    assert recipes != []
+
 
